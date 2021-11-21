1edb97e28bfea9febb7bb70b77301c91d82bf7dc
benakli <william.benakli.4.2@gmail.com>
2021-11-21 20:01:00 +0100

    creation de la command createCommandLine

--- src/utils/command_line.c
+++ src/utils/command_line.c
@@ -1,1 +1,11 @@
 #include "utils/command_line.h"
+
+int create_commandline(struct commandline *command, char *argv){
+  uint32_t len = strlen(str);
+  if(len >= 2){
+    return -1;
+  }
+  command->ARGC = len;
+  command->ARGV = argv;
+  return 0;
+}
