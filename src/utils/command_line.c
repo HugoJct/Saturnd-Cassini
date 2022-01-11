0ac86c78fe0abe0c76c825f69c695c161ca4a0fc
benakli <william.benakli.4.2@gmail.com>
2022-01-11 14:10:00 +0100

    fonction qui attendra les données du fichier pour les transformer en **cmd

--- src/utils/command_line.c
+++ src/utils/command_line.c
@@ -58,3 +58,29 @@ char **arg_array_from_buf(char *buf) {
 
 	return argv;
 }
+
+char **arg_array_from_buf_file(char *str) {
+
+	int init_size = strlen(str);
+	char delim[] = " ";
+
+	char *ptr = strtok(str, delim);
+
+	while(ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
+
+	/* This loop will show that there are zeroes in the str after tokenizing */
+	for (int i = 0; i < init_size; i++)
+	{
+		printf("%d ", str[i]); /* Convert the character to integer, in this case
+							   the character's ASCII equivalent */
+	}
+	printf("\n");
+
+	return 0;
+
+	return argv;
+}
